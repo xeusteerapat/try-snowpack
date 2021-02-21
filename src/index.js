9e55c19b20ebf7f295e2bca02cce3689504672d6
xeusteerapat <xeus085@gmail.com>
2021-02-21 20:23:30 +0700

    [ADD] add html js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { add, subtract, multiply, divide } from './math';
+
+const add45 = add(4, 5);
+const sub45 = subtract(4, 5);
+const mul45 = multiply(4, 5);
+const div45 = divide(4, 5);
+
+console.log(add45);
+console.log(sub45);
+console.log(mul45);
+console.log(div45);
